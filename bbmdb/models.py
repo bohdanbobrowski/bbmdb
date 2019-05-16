66f7ec200d42b2bf81776f9f231538eaec13f072
Bohdan Bobrowski <bohdan@bobrowski.com.pl>
2019-05-16 21:48:32 +0200

    Finnaly I can retrieve data from OMDB :-P

--- bbmdb/models.py
+++ bbmdb/models.py
@@ -3,14 +3,27 @@
 # author: Bohdan Bobrowski bohdan@bobrowski.com.pl
 
 from django.db import models
+import omdb
+
+API_KEY='95667d5d'
+omdb.set_default('apikey', API_KEY)
 
 
 class Movies(models.Model):
     movie_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255, null=False)
-    year = models.IntegerField()
-    imdb_rating = models.FloatField(max_length=1)
-    director = models.CharField(max_length=255, null=False)
+    year = models.IntegerField(null=True)
+    imdb_rating = models.FloatField(max_length=1, null=True)
+    director = models.CharField(max_length=255, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.movie_id:
+            omdb_data = omdb.title(self.title)
+            if omdb_data:
+                self.year = omdb_data['year']
+                self.imdb_rating = omdb_data['imdb_rating']
+                self.director = omdb_data['director']
+        super(Movies, self).save(*args, **kwargs)
 
     def __str__(self):
         return "{}. {} [{}] {} - {}/10".format(self.movie_id, self.title, self.year, self.imdb_rating, self.director)
